64ef0468f57283b5fa90d47e17fe1dbc078c74a2
Zhehao Mao <zhehao.mao@gmail.com>
2012-04-28 08:36:20 -0400

    bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='bootlegger',
-    version='0.1.0',
+    version='0.2.0',
     description='Command Line Interface to Speakeasy',
     author='Zhehao Mao',
     author_email='zhehao.mao@gmail.com',
